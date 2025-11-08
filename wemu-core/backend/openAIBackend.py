45e96c5157503c33acca60556da0725c588ed54c
Aerodream Granite <yxdowncomputer@outlook.com>
2025-11-08 06:50:08 +0000

    finish OpenAI backend (and update gitignore)

--- wemu-core/backend/openAIBackend.py
+++ wemu-core/backend/openAIBackend.py
@@ -1,5 +1,5 @@
 import openai
-def openAIGenerate(history:list,newMsg:str,config:dict)->tuple[list,str]:
+def openAIGenerate(history:list,newMsg:str,config:dict)->tuple[list,str,str]:
     client=openai.OpenAI(api_key=config['apiKey'],base_url=config['baseURL'])
     if newMsg!="":
         history.append({'role':'user','content':newMsg})
@@ -9,10 +9,15 @@ def openAIGenerate(history:list,newMsg:str,config:dict)->tuple[list,str]:
         stream=True
     )
     aiResponce=""
+    aiThinking=""
     for chunk in respond:
         aiResponce+=chunk.choices[0].delta.content
+        if hasattr(chunk.choices[0].delta,"reasoning"):
+            aiThinking+=chunk.choices[0].delta.reasoning
+        elif hasattr(chunk.choices[0].delta,"reasoning_content"):
+            aiThinking+=chunk.choices[0].delta.reasoning_content
     history.append({'role':'assistant','content':aiResponce})
-    return history,aiResponce
+    return history,aiResponce,aiThinking
 def main():
     print("OpenAI backend testing...")
     print("Test 01: Non-thinking model")
@@ -21,12 +26,14 @@ def main():
         "baseURL":"http://192.168.3.232:11434/v1",
         "model":"qwen3-2507-instruct:30b-q2k"
     }
-    history,aiResponce=openAIGenerate(history=[],newMsg="随便写点什么,什么都行,长度任意,最好带几个换行",config=cfg)
+    history,aiResponce,aiThinking=openAIGenerate(history=[],newMsg="随便写点什么,什么都行,长度任意,最好带几个换行",config=cfg)
+    print("AI's thinking:",aiThinking)
     print("AI's responce:",aiResponce)
     print("History set:",history)
     print("Test 02: Thinking model")
     cfg["model"]="qwen3-2507-thinking:30b-q2k"
-    history,aiResponce=openAIGenerate(history=[],newMsg="随便写点什么,什么都行,长度任意,最好带几个换行",config=cfg)
+    history,aiResponce,aiThinking=openAIGenerate(history=[],newMsg="随便写点什么,什么都行,长度任意,最好带几个换行",config=cfg)
+    print("AI's thinking:",aiThinking)
     print("AI's responce:",aiResponce)
     print("History set:",history)
 
